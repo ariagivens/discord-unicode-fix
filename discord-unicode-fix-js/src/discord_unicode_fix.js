25a137913cd1af58afdacba4d94c961391ee9e0a
Aria Givens <aria@ariagivens.com>
2025-04-08 22:15:06 -0400

    Remove dependency on Grapheme-Splitter

--- discord-unicode-fix-js/src/discord_unicode_fix.js
+++ discord-unicode-fix-js/src/discord_unicode_fix.js
@@ -1,26 +1,26 @@
-import unicode_map from './unicode_map.csv';
-import GraphemeSplitter from 'grapheme-splitter';
-const splitter = new GraphemeSplitter();
+import unicode_map from "./unicode_map.csv";
+const segmenter = new Intl.Segmenter(undefined, { granularity: "grapheme" });
 
 /**
-* @param {string} broken
-* @returns {string}
-*/
+ * @param {string} broken
+ * @returns {string}
+ */
 export function fix(broken) {
-    let fixed = "";
+  let fixed = "";
 
-    outer: while (broken.length > 0) {
-        for ( const { expected, actual } of unicode_map.values() ) {
-            if (broken.startsWith(actual)) {
-                fixed += expected;
-                broken = broken.slice(actual.length);
-                continue outer;
-            }
-        }
-        let grapheme = splitter.iterateGraphemes(broken).next().value;
-        fixed += grapheme;
-        broken = broken.slice(grapheme.length);
+  outer: while (broken.length > 0) {
+    for (const { expected, actual } of unicode_map.values()) {
+      if (broken.startsWith(actual)) {
+        fixed += expected;
+        broken = broken.slice(actual.length);
+        continue outer;
+      }
     }
+    let grapheme = segmenter.segment(broken)[Symbol.iterator]().next()
+      .value.segment;
+    fixed += grapheme;
+    broken = broken.slice(grapheme.length);
+  }
 
-    return fixed;
+  return fixed;
 }
